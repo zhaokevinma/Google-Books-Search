698acc47cf8836f4733b4063188f7c35ed4b1fca
Kevin Malone <zhaokevinma@gmail.com>
2019-11-09 22:13:40 -0500

    Not ready to merge - error build index.html missing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,11 @@
 const axios = require("axios");
 const router = require("express").Router();
+const queryURL = "https://www.googleapis.com/books/v1/volumes/q=";
+const API_KEY = process.env.API_KEY;
 
 router.get("/recipes", (req, res) => {
   axios
-    .get("http://www.recipepuppy.com/api/", { params: req.query })
+    .get(`${queryURL}${req.query }&key=${API_KEY}`)
     .then(({ data: { results } }) => res.json(results))
     .catch(err => res.status(422).json(err));
 });
--- server.js
+++ server.js
@@ -1,19 +1,26 @@
+// ------ Dependencies ------
 const express = require("express");
 const path = require("path");
+const mongoose = require('mongoose');
 
+// ------ Environmental Variables ------
 const PORT = process.env.PORT || 3001;
-const app = express();
 const apiRoutes = require("./routes/apiRoutes");
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/googlebooks';
 
-// Define middleware here
+// ------ Create App -------
+const app = express();
+
+// ------ Define Middleware Here ------
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
+
+// ------ Serve Up Static Assets (usually on heroku) ------
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Use apiRoutes
+// ------ Use apiRoutes ------
 app.use("/api", apiRoutes);
 
 // Send every request to the React app
@@ -22,6 +29,10 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+// ------ Connect to Database ------
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+
+// ------ Start App ------
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
